b06001523c38f244de6b2a86dc2a1420f27c34e1
Manas <mpmulay971@gmail.com>
2025-04-11 15:10:27 +0530

    Added function to check bitwise operations

--- calc.c
+++ calc.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
+
+bool even (int n){
+	return !(n & 1);
+}
+bool odd (int n){
+	return (n & 1);
+}
+
 
 int main() {
     char mode;
@@ -102,6 +111,17 @@ int main() {
         printf("Invalid mode selected.\n");
     }
 
+	int oddn;
+	printf("Enter the number to check if it is odd : ");
+	scanf("%d", &oddn);
+	if(odd(oddn)) printf("The number %d is odd", oddn);
+	else printf("The number %d is even", oddn);
+
+	int evenn;
+	printf("Enter the number to check if it is even : ");
+	scanf("%d", &evenn);
+	if(even(evenn)) printf("The number %d is even", evenn);
+	else printf("The number %d is odd", evenn);
     return 0;
 }
 
